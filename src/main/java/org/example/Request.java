c3983034b781b9902d0e6bd33d3dac8119279821
gigascript <gigascript@yandex.ru>
2022-12-15 21:50:55 +0700

    Добавлена getPostParam() и getPostParams() для контента тип x-www-form-url-encoded

--- src/main/java/org/example/Request.java
+++ src/main/java/org/example/Request.java
@@ -1,10 +1,14 @@
 package org.example;
 
 import org.apache.hc.core5.http.NameValuePair;
+import org.apache.hc.core5.http.io.entity.StringEntity;
+import org.apache.hc.core5.http.message.BasicNameValuePair;
 import org.apache.hc.core5.net.URIBuilder;
+import org.apache.hc.core5.net.URLEncodedUtils;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.*;
 
@@ -17,6 +21,7 @@ public class Request {
     private final URIBuilder builder;
     private final String body;
     private final List<NameValuePair> queryParams;
+    private final List<NameValuePair> postParams;
 
 
     public Request(String method, String fullPath, List<String> headers, String body) throws URISyntaxException {
@@ -27,11 +32,36 @@ public class Request {
         this.queryParams = builder.getQueryParams();
         this.headers = headers;
         this.body = body;
+        this.postParams = setPostParam();
     }
 
+    private List<NameValuePair> setPostParam() {
+        if (!getMethod().equals("GET")) {
+            final Optional<String> contentType = getHeader("Content-Type");
+            if (contentType.isPresent()) {
+                final String contentTypeValue = contentType.get();
+                if (contentTypeValue.contains("x-www-form-urlencoded")) {
+                    return URLEncodedUtils.parse(getBody(), StandardCharsets.UTF_8);
+                }
+            }
+        }
+        return new ArrayList<>();
+    }
+
+    public Optional<String> getHeader(String header) {
+        return headers.stream()
+                .filter(o -> o.startsWith(header))
+                .map(o -> o.substring(o.indexOf(" ")))
+                .map(String::trim)
+                .findFirst();
+    }
 
     public List<NameValuePair> getQueryParams() {
         return queryParams;
+    }
+
+    public List<NameValuePair> getPostParams() {
+        return postParams;
     }
 
     public NameValuePair getQueryParam(String name) {
@@ -39,12 +69,35 @@ public class Request {
             System.err.print("[Запрошен QueryParam = " + name + "] QueryParam пуст \r\n");
             return null;
         }
-        NameValuePair nameValuePair = builder.getFirstQueryParam(name);
-        System.out.println();
+        NameValuePair nameValuePair = findParamByName(name, queryParams);
         if (nameValuePair != null) {
             System.out.print("[Запрошен QueryParam = " + name + "] Param value = " + nameValuePair.getValue() + "\r\n");
         } else {
             System.err.print("[Запрошен QueryParam = " + name + "] QueryParam не найден \r\n");
+        }
+        return nameValuePair;
+    }
+
+    public NameValuePair getPostParam(String name) {
+        if (postParams.isEmpty()) {
+            System.err.print("[Запрошен QueryParam = " + name + "] QueryParam пуст \r\n");
+            return null;
+        }
+        NameValuePair nameValuePair = findParamByName(name, postParams);
+        if (nameValuePair != null) {
+            System.out.print("[Запрошен PostParam = " + name + "] Param value = " + nameValuePair.getValue() + "\r\n");
+        } else {
+            System.err.print("[Запрошен PostParam = " + name + "] PostParam не найден \r\n");
+        }
+        return nameValuePair;
+    }
+
+    private NameValuePair findParamByName(String name, List<NameValuePair> params) {
+        NameValuePair nameValuePair = null;
+        for (NameValuePair param : params) {
+            if (param.getName().equals(name)) {
+                nameValuePair = param;
+            }
         }
         return nameValuePair;
     }
@@ -71,6 +124,7 @@ public class Request {
                 ", \n headers='" + headers + '\'' +
                 ", \n body='" + body + '\'' +
                 ", queryParams=" + getQueryParams() +
+                ", postParams=" + getPostParams() +
                 "}]";
     }
 }
